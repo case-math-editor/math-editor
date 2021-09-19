3b7c3261ab89f8f1daf75d6f53f0044e5e12f8e0
Sergey Zhurbey <zhurbeysa@yandex-team.ru>
2021-09-19 19:03:06 +0300

    init commit

---
+++ tests/test_utils/test_matrices.py
@@ -0,0 +1,20 @@
+import pytest
+
+from math_editor.utils.matrices import is_present_in_matrix
+
+
+TEST_IS_PRESENT_IN_MATRIX_DATA = (
+    # test args scheme: "matrix,target,result"
+    ([[1]], 1, True),
+    ([[1]], 10, False),
+    ([[1]], -1, False),
+)
+
+
+@pytest.mark.utils
+@pytest.mark.parametrize("matrix,target,result", TEST_IS_PRESENT_IN_MATRIX_DATA)
+def test_is_present_in_matrix(
+    matrix: list[list[int]], target: int, result: bool
+) -> None:
+
+    assert is_present_in_matrix(matrix, target) == result
